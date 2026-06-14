21996827aa42dec291e1bd5022e1cc079164224b
onvot0346-debug <onvot0346@gmail.com>
2026-06-14 16:20:31 +0300

    Update scroll-area.jsx

--- frontend/src/components/ui/scroll-area.jsx
+++ frontend/src/components/ui/scroll-area.jsx
@@ -1,7 +1,8 @@
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
-import { cn } from "@/lib/utils"
+// REİS: HATA BURADAYDI, DÜZELTİLDİ.
+import { cn } from "../../lib/utils"
 
 const ScrollArea = React.forwardRef(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
